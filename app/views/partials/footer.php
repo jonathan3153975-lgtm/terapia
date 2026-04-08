179e27b66496e617835b3ed4617ca26d0a850201
SSADESC <cor-sat@bm.rs.gov.br>
2026-04-08 14:25:13 -0300

    ajuste

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -3,11 +3,20 @@
 </div>
 <?php endif; ?>
 
+<?php
+$assetBase = rtrim($appUrl ?? '', '/');
+if ($assetBase === '') {
+	$scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? ''));
+	$scriptDir = rtrim($scriptDir, '/');
+	$assetBase = ($scriptDir === '.' || $scriptDir === '') ? '' : $scriptDir;
+}
+?>
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
-<script src="<?php echo $appUrl; ?>/public/js/app.js"></script>
+<script src="<?php echo $assetBase; ?>/public/js/app.js"></script>
 <script>
 (function() {
 	const stored = localStorage.getItem('theme-mode');
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -3,6 +3,13 @@
 /** @var string $appUrl */
 $title = $title ?? 'Terapia SaaS';
 $appUrl = $appUrl ?? '';
+
+$assetBase = rtrim($appUrl, '/');
+if ($assetBase === '') {
+  $scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? ''));
+  $scriptDir = rtrim($scriptDir, '/');
+  $assetBase = ($scriptDir === '.' || $scriptDir === '') ? '' : $scriptDir;
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -13,6 +20,6 @@ $appUrl = $appUrl ?? '';
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
   <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
-  <link href="<?php echo $appUrl; ?>/public/css/app.css" rel="stylesheet">
+  <link href="<?php echo $assetBase; ?>/public/css/app.css" rel="stylesheet">
 </head>
 <body class="app-body">
