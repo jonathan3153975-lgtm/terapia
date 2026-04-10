f4a7cbe197068f5300d2e327c3ce3b5881841013
SSADESC <cor-sat@bm.rs.gov.br>
2026-04-10 17:32:46 -0300

    ajustes

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+$autoload = __DIR__ . '/vendor/autoload.php';
+if (!file_exists($autoload)) {
+    $autoload = __DIR__ . '/autoload.php';
+}
+
+if (!file_exists($autoload)) {
+    http_response_code(500);
+    echo 'Erro de inicializacao: arquivo de autoload nao encontrado.';
+    exit;
+}
+
+require $autoload;
 
 use App\Controllers\AuthController;
 use Config\Config;
